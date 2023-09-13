0f81696ec3fd50718f569e6df62ca00168b3682b
Ashwani Pandey <ashwanipandey679@gmail.com>
2023-09-14 01:26:32 +0530

    Registration Successfull

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import axios from "axios"
 
 export const Auth = ()=>{
     return(
@@ -29,6 +30,22 @@ const Register = ()=>{
     const [username , setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const onSubmit = async (event)=>{
+        event.preventDefault();
+        try {
+            axios.post("http://localhost:3001/auth/register", {
+                username, password
+            });
+            alert("Registration Completed! Now Login..")
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+
+
+    };
     return(
         <Form 
             username={username}
@@ -36,24 +53,45 @@ const Register = ()=>{
             password={password}
             setPassword={setPassword}
             label="Register"
+            onSubmit={onSubmit}
 
         />
     );
     
 };
 
-const Form = ({username , setUsername, password, setPassword, label}) =>{
+const Form = ({
+            username, 
+            setUsername, 
+            password, 
+            setPassword, 
+            label,
+            onSubmit,
+
+        }) =>{
     return(
         <div className="auth-container">
-            <form action="">
+            <form onSubmit={onSubmit}>
                 <h2 >{label}</h2>
                 <div className="form-group">
                     <label htmlFor="userame" >Username</label>
-                    <input type="text" id="username" onChange={(event)=>{}} />
+                    <input 
+                        type="text" 
+                        id="username"
+                        value={username} 
+                        onChange={(event)=>{
+                            setUsername(event.target.value)}} 
+                            />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password" >Password</label>
-                    <input type="text" id="Password" onChange={(event)=>{}} />
+                    <input 
+                    type="password" 
+                    id="Password" 
+                    value={password}
+                    onChange={(event)=>{
+                        setPassword(event.target.value)
+                        }} />
                 </div>
                 <button className="btn btn-primary">{label}</button>
             </form>
